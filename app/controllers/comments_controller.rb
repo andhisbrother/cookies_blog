f92d4ecc4ab6c020d6fba39087aac166ced72fcb
Tomas Stanko <tomas.stanko1000@gmail.com>
2013-05-15 23:34:15 +0200

    comments adds

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,18 @@
 class CommentsController < ApplicationController
+  
   def new
-  	
+  	@post = Post.find params[:post_id]
+  	@comment = @post.comments.new
   end
+
+  def create
+  	@post = Post.find params[:post_id]
+  	@comment = @post.comments.build params[:comment]
+  	if @comment.save
+  		redirect_to post_path(@post)
+  	else
+  		render :new
+  	end
+  end
+
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
 	belongs_to :post
   attr_accessible :author, :comment_body, :email, :post_id
+  validates :author, :comment_body, :email, :presence => true
 end
